b26ca962eb641102fdcac448de5e22eeeee85e1f
sea <78116143+Myokhant5306@users.noreply.github.com>
2021-12-15 14:30:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import bodyParser from 'body-parser';
 import m from 'mongoose';
 import crypto from 'crypto';
 import http from 'http';
-import CORS from './CORS.js';
 import UserModel from './models/User.js';
 const User = UserModel(m);
 const app = appSrc(express, bodyParser, fs, crypto, http, CORS, User, m);
