5c0ea7353195956bb5118640f852feeb1e80c00f
sea <78116143+Myokhant5306@users.noreply.github.com>
2021-12-16 00:22:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,69 +1,58 @@
 
-export default function appScr(express, bodyParser, fs, crypto, http, CORS, User, m) {
+export default (express, bodyParser, createReadStream, crypto, http, m, UserSchema, writeFileSync) => {
     const app = express();
-    const hu = {'Content-Type':'text/html; charset=utf-8'}
+
+    const User = m.model('User', UserSchema);
+    
     const CORS = {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE',
-      };
-    let headers = {
-        'Content-Type':'text/plain',
-        ...CORS
-    }
-    return app
-        .use(bodyParser.urlencoded({extended:true}))       
-        .all('/login/', r => {
-            r.res.set(headers).send('myokhant');
-        })
-        .all('/code/', r => {
-            r.res.set(headers)
-            fs.readFile(import.meta.url.substring(7),(err, data) => {
-                if (err) throw err;
-                r.res.end(data);
-              });           
-        })
-        .all('/sha1/:input/', r => {
-            r.res.set(headers).send(crypto.createHash('sha1').update(r.params.input).digest('hex'))
-        })
-        .get('/req/', (req, res) =>{
-            res.set(headers);
-            let data = '';
-            http.get(req.query.addr, async function(response) {
-                await response.on('data',function (chunk){
-                    data+=chunk;
-                }).on('end',()=>{})
-                res.send(data)
-            })
-        })
-         .post('/req/', (req, res) =>{
-            res.set(headers);
-            let data = '';
-            http.get(req.body.addr, async function(response) {
-                await response.on('data',function (chunk){
-                    data+=chunk;
-                }).on('end',()=>{})
-                res.send(data)
-            })
-        })
-        .post('/insert/', async r=>{
-            r.res.set(headers);
-            const {login,password,URL}=r.body;
-            const newUser = new User({login,password});
-            try{
-                await m.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true});
-                try{
-                    await newUser.save();
-                    r.res.status(201).json({'Created: ':login});
-                }
-                catch(e){
-                    r.res.status(400).json({'Error: ':'Incorrect password'});
-                }
-            }
-            catch(e){
-                console.log(e.codeName);
-            }      
-        })
-        .use(({res:r})=>r.status(404).set(hu).send('myokhant'))
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers':'x-test,Content-Type,Accept, Access-Control-Allow-Headers'
+        }; 
+    
+    app
+    
+    .use((r, res, next) => { r.res.set(CORS); next(); })
+    .use(bodyParser.urlencoded({ extended: true }))
+    .get('/sha1/:input', r => {
+        const shasum = crypto.createHash('sha1');
+        shasum.update(r.params.input);
+    
+        r.res.send(shasum.digest('hex'));
+    })
+    
+    .get('/login/', (req, res) => res.send('myokhant'))
+    .get('/code/', (req, res) => {
+        res.set({'Content-Type': 'text/plain; charset=utf-8'});
+        createReadStream(import.meta.url.substring(7)).pipe(res);
+    })
+    ;
+    .post('/insert/', async (req, res) => {
+        const { URL, login, password } = req.body;
+        try {
+          await m.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        } catch (e) {
+          res.send(e.stack);   
+        }
+
+        const newUser = new User({ login, password });
+        await newUser.save();
+        res.status(201).json({ successsss: true, login });
+    }); 
+    
+
+   // .use(({res:r})=>r.status(404).set(headersHTML).send('myokhant'))
+    
+    .all('/req/', (req, res) => {
+        const addr = req.method === 'POST' ? req.body.addr : req.query.addr;
+
+        http.get(addr, (r, b = '') => {
+            r
+            .on('data', d => b += d)
+            .on('end', () => res.send(b));
+        });
+    })
+    
     return app;
 }
 
